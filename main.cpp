1f9ec2a501bed9e102677e50e80e1a1b0a2d7b37
nban22 <nban22@clc.fitus.edu.vn>
2023-03-26 23:40:44 +0700

    change the menu in main.cpp, midified students.csv and teachers.csv

--- main.cpp
+++ main.cpp
@@ -10,9 +10,7 @@ int main()
 	ifstream input;
 	STFF_NODE* staff = nullptr; 
 	getDataTeachers_csv(input, staff);
-	//STFF_NODE* staff2 = staff;
-	//print_Staffs(staff);  //for testing
-	//system("pause");
+	STFF_NODE* loggedinStaff = new STFF_NODE;
 
 	//ACCOUNT TO TEST RUN: 
 	//USER:   24061530
@@ -21,15 +19,13 @@ int main()
 	ifstream input_S;
 	STU_NODE* student = nullptr;
 	getDataStudents_csv(input_S, student);
-	/*STU_NODE* student2 = student;*/
+	STU_NODE* loggedinStudent = new STU_NODE;
 
 	ifstream input_C;
 	CR_NODE* course = nullptr;
 	STU_COURSE_NODE *stu_course = nullptr;
 	Get_Data_StudentCourse_csv(input_C, stu_course);
 
-	STFF_NODE* loggedinStaff = new STFF_NODE;
-	STU_NODE* loggedinStudent = new STU_NODE;
 
 	while (69) {
 		int check_T, check_S;
@@ -65,19 +61,26 @@ int main()
 			system("cls");
 
 			cout << "======================TEACHER======================";
-			cout << "\n\t1. Change password.\n"
-				<< "\t2. Update your persinal information.\n"
-				<< "\t3. Create a school year.\n"
-				<< "\t4. Add new 1st year students to 1st year classes.\n"
-				<< "\t5. Create a course registration session.\n"
-				<< "\t6. Export list of student in a course.\n"
-				<< "\t7. Enter and view the scoreboard of a course.\n"
-				<< "\t8. View the scoreboard of a course.\n"
-				<< "\t9. Import the scoreboard of a course.\n"
-				<< "\t10. Update a student result.\n"
-				<< "\t11. View the scoreboard of a class.\n"
-				<< "\t0. Log out.\n"
-				<< "\t-1. Exit\n";
+			//cout << "\n\t1. Change password.\n"
+			//	<< "\t2. Update your persinal information.\n"
+			//	<< "\t3. Create a school year.\n"
+			//	<< "\t4. Add new 1st year students to 1st year classes.\n"
+			//	<< "\t5. Create a course registration session.\n"
+			//	<< "\t6. ...... a scoreboard of a course.\n"
+			//			//	<< "\t1. Enter and view the scoreboard of a course.\n"
+			//			//	<< "\t2. View the scoreboard of a course.\n"
+			//			//	<< "\t3. Import the scoreboard of a course.\n"
+			//	<< "\t7. View the scoreboard of a course.\n"
+			//	<< "\t8. Update a student result.\n"
+			//	<< "\t9. View the scoreboard of a class.\n"
+			//	<< "\t0. Log out.\n"
+			//	<< "\t-1. Exit\n";
+			cout << "\n\t1.Create new."//Tao moi
+				<< "\n\t2.Enter information."//Nhap thong tin
+				<< "\n\t3.Look up."//Tra cuu
+				<< "\n\t4.Change password."//Doi mat khau
+				<< "\n\t0.Log out."//Dang xuat
+				<< "\n\t-1.Exit.\n";
 			cout << "========================END========================\n\n";
 
 			cout << "Teacher.\n";
@@ -92,48 +95,108 @@ int main()
 			cin >> choose;
 			cin.ignore();
 
-			if (choose == 1) {
-				changePassWordOfStaffAccount(staff, loggedinStaff);
-			}
-			else if (choose == 2) {
+			if (choose == 1) { //Create new
+				system("cls");
+				cout << "======================Look up======================";
+				cout << "\n\t1.Create a new school."
+					<< "\n\t2.Create semester."
+					<< "\n\t3.List of courses."
+					<< "\n\t0.Come back.\n";
+				cout << "========================END========================\n\n";
+				cin >> choose;
+				cin.ignore();
+				if (choose == 1) {
 
-			}
-			else if (choose == 3) {
+				}
+				else if (choose == 2) {
 
+				}
+				else if (choose == 3) {
+
+				}
+				else if (choose == 4) {
+
+				}
+				else if (choose == 0) {
+					break;
+				}
+				else {
+					cout << "Invalid selection, please enter again.\n\n";
+					system("pause");
+				}
+			}
+			else if (choose == 2) { //Enter information
+				system("cls");
+				cout << "======================Look up======================";
+				cout << "\n\t1.Add new 1st year students to 1st year classes."//them sinh vien nam nhat vao lop
+					<< "\n\t2.Create a course registration session."//tao phien DKHP
+					<< "\n\t3.Add a course."//Them khoa hoc
+					<< "\n\t4.Export the file to import the list of students in each class."//Xuat file nhap DSSV tung lop
+					<< "\n\t5.Delete a course."//Xoa khoa hoc
+					<< "\n\t6.Update a course."//Cap nhat khoa hoc
+					<< "\n\t7.update the marks in a course."//Cap nhat diem trong khoa hoc
+					<< "\n\t0.Come back.\n";
+				cout << "========================END========================\n\n";
+				cin >> choose;
+				cin.ignore();
+				if (choose == 1) {
+
+				}
+				else if (choose == 2) {
+
+				}
+				else if (choose == 3) {
+
+				}
+				else if (choose == 4) {
+
+				}
+				else if (choose == 0) {
+					break;
+				}
+				else {
+					cout << "Invalid selection, please enter again.\n\n";
+					system("pause");
+				}
+			}
+			else if (choose == 3) { //Look up
+				system("cls");
+				cout << "======================Look up======================";
+				cout << "\n\t1.List of classes." //Danh sach lop
+					<< "\n\t2.List of students in class." //Danh sach sinh vien trong lop
+					<< "\n\t3.List of courses." // Danh sach khoa hoc
+					<< "\n\t4.List of students in a course." //Danh sach sinh vien trong trong khoa hoc
+					<< "\n\t5.View a scoreboard in a course." //Xem bang diem trong khoa hoc
+					<< "\n\t6.View a scoreboard in class." //Xem bang diem trong lop
+					<< "\n\t4.Export csv file about a scoreboard of students in course to enter score."
+					//Xuat csv file bang diem sinh vien trong khoa hoc de nhap diem
+					<< "\n\t0.Come back.\n";
+				cout << "========================END========================\n\n";
+				cin >> choose;
+				cin.ignore();
+				if (choose == 1) {
+					/*STU_NODE* listclass = new STU_NODE;*/
+					
+				}
+				else if (choose == 2) {
+
+				}
+				else if (choose == 3) {
+
+				}
+				else if (choose == 4) {
+
+				}
+				else if (choose == 0) {
+					break;
+				}
+				else {
+					cout << "Invalid selection, please enter again.\n\n";
+					system("pause");
+				}
 			}
 			else if (choose == 4) {
-
-			}
-			else if (choose == 5) {
-				DisplayMenu(choose);
-			}
-			else if (choose == 6) {
-
-			}
-			else if (choose == 7) {
-				int check;
-				system("cls");
-				cout << "\n\t\t\t ********************************************" << endl;
-				cout << "\t\t\t\t      ";
-				cout << "Enter the scoreboard of a course." << endl;
-				EnterCourseScore(stu_course, course, loggedinStaff, check);
-				if (check != 0) {
-					Read_After_Update_Student_Course(stu_course);
-					count = 7;
-				}
-				system("pause");
-			}
-			else if (choose == 8) {
-
-			}
-			else if (choose == 9) {
-
-			}
-			else if (choose == 10) {
-
-			}
-			else if (choose == 11) {
-
+				changePassWordOfStaffAccount(staff, loggedinStaff);
 			}
 			else if (choose == 0) {
 				break;
@@ -148,6 +211,56 @@ int main()
 			}
 		}
 
+		//	if (choose == 1) {
+		//		changePassWordOfStaffAccount(staff, loggedinStaff);
+		//	}
+		//	else if (choose == 2) {
+
+		//	}
+		//	else if (choose == 3) {
+
+		//	}
+		//	else if (choose == 4) {
+
+		//	}
+		//	else if (choose == 5) {
+		//		DisplayMenu(choose);
+		//	}
+		//	else if (choose == 6) { // ong lam cai menu o day nhe.
+		//		int check;
+		//		system("cls");
+		//		cout << "\n\t\t\t ********************************************" << endl;
+		//		cout << "\t\t\t\t      ";
+		//		cout << "Enter the scoreboard of a course." << endl;
+		//		EnterCourseScore(stu_course, course, loggedinStaff, check);
+		//		if (check != 0) {
+		//			Read_After_Update_Student_Course(stu_course);
+		//			count = 7;
+		//		}
+		//		system("pause");
+		//	}
+		//	else if (choose == 7) {
+		//		
+		//	}
+		//	else if (choose == 8) {
+
+		//	}
+		//	else if (choose == 9) {
+
+		//	}
+		//	else if (choose == 0) {
+		//		break;
+		//	}
+		//	else if (choose == -1) {
+		//		system("pause");
+		//		return 0;
+		//	}
+		//	else {
+		//		cout << "Invalid selection, please enter again.\n\n";
+		//		system("pause");
+		//	}
+		//}
+
 		//THE FUNCTIONS OF STUDENT
 		while (check_S == 1) {
 			system("cls");
@@ -156,7 +269,7 @@ int main()
 				<< "2. Update your personal information.\n"
 				<< "3. Register for the course.\n"
 				<< "4. View your schedule.\n"
-				<< "5. View your scoraboard."
+				<< "5. View your scoraboard.\n"
 				<< "6. View list of student in a course.\n"
 				<< "7. View of classes.\n"
 				<< "8. View list of student in a class.\n"
